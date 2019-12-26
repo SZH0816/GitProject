d2c79404fe9fd4d3fb6779c71da81f8fde5f790e
baekhyun <baekhyun>
2019-12-26 11:29:03 +0800

    合并分支

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("mmas");
         System.out.println("dev");
         System.out.println("dev");
     }
