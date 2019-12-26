30b3ce2ddedda9bf76d9e0c35f6b481335d1d20c
baekhyun <baekhyun>
2019-12-26 11:19:22 +0800

    添加test工程至git

--- src/com/company/test.java
+++ src/com/company/test.java
@@ -2,12 +2,6 @@ package com.company;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("修改");
-
+        System.out.println("master");
     }
 }
